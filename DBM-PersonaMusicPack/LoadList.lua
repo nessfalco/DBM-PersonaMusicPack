13734a7a0141a2ea2b33964f6a9cb789cd89f19e
Nessfalco <nessfalco2@gmail.com>
2021-01-28 16:04:00 -0500

    Initial Commit

---
+++ DBM-PersonaMusicPack/LoadList.lua
@@ -0,0 +1,245 @@
+local insert = table.insert
+local inserted1, inserted2 = false, false
+
+-- Try to use unique function names so diff user packs don't overwrite eachother, Generally I just use MP, VP, DF + Unique Addon Name
+-- Function names are defined in TOC. A pack does not need to contain all 3 sound types. You are welcome to make just specific ones, just make sure TOC is correct
+
+function DBMVPPersonaMusicPack() -- Register Victory sounds to DBM.Victory table
+	if inserted1 then
+		return
+	end
+	insert(DBM.Victory, {
+		text	= "Persona 5 - Triumph",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Victory\\Persona 5 - Triumph.ogg"
+	})
+	insert(DBM.Victory, {
+		text	= "Persona 3 - After the Battle",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Victory\\Persona 3 - After the Battle.ogg"
+	})
+	insert(DBM.Victory, {
+		text	= "Persona 4 - Victory",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Victory\\Persona 4 - Victory.ogg"
+	})
+
+	inserted1 = true
+end
+
+-- function DBMDPPersonaMusicPack() -- Register Defeat sounds to DBM.Defeat Table
+-- 	if inserted2 then
+-- 		return
+-- 	end
+-- 	insert(DBM.Defeat, {
+-- 		text	= "Alex Death",
+-- 		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Defeat\\alexdeath.ogg"
+-- 	})
+-- 	inserted2 = true
+-- end
+
+function DBMMPPersonaMusicPack() -- Register Music to DBM.Music Table
+	if inserted2 then
+		return
+	end
+	-- All Music Table
+	insert(DBM.Music, {
+		text	= "Persona 5 - Last Surprise",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 - Last Surprise.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 5 - Life Will Change",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 - Life Will Change.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 5 - Rivers In the Desert",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 - Rivers In the Desert.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 5 - Will Power",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 - Will Power.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 5 Royal - I Believe",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 Royal - I Believe.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 5 Royal - Kichijoji 199X",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 Royal - Kichijoji 199X.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 5 Royal - Take Over",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 Royal - Take Over.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 3 - Burn My Dread",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 3 - Burn My Dread.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 3 - Mass Destruction",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 3 - Mass Destruction.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 3 - Tartarus",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 3 - Tartarus.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 3 - When The Moon's Reaching Out Stars",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 3 - When The Moon's Reaching Out Stars.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 4 - I'll Face Myself-Battle",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - I'll Face Myself-Battle.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 4 - Pursuing My True Self",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Pursuing My True Self.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 4 - Reach Out To The Truth",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Reach Out To The Truth.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 4 - Time to Make History",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Time to Make History.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 4 - Heaven",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Heaven.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 4 - Heartbeat, Heartbreak",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Heartbeat, Heartbreak.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Persona 4 - Signs of Love",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Signs of Love.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Final Fantasy XIV OST - Shiva First Phase Theme [Footsteps in the Snow]",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Final Fantasy XIV OST - Shiva First Phase Theme [Footsteps in the Snow].ogg"
+	})
+	insert(DBM.Music, {
+		text	= "FFXIV Shadowbringers - Insatiable",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\FFXIV Shadowbringers - Insatiable.ogg"
+	})
+	insert(DBM.Music, {
+		text	= "Golden Sun - Battle Theme",
+		value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Golden Sun - Battle Themee.ogg"
+	})
+	
+
+
+
+	-- Dungeon BGM Table
+	if DBM.DungeonMusic then
+		insert(DBM.DungeonMusic, {
+			text	= "Persona 3 - Tartarus",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 3 - Tartarus.ogg"
+		})
+		insert(DBM.DungeonMusic, {
+			text	= "Persona 3 - When The Moon's Reaching Out Stars",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 3 - When The Moon's Reaching Out Stars.ogg"
+		})
+		insert(DBM.DungeonMusic, {
+			text	= "Persona 4 - Heaven",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Heaven.ogg"
+		})
+		insert(DBM.DungeonMusic, {
+			text	= "Persona 4 - Heartbeat, Heartbreak",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Heartbeat, Heartbreak.ogg"
+		})
+		insert(DBM.DungeonMusic, {
+			text	= "Persona 4 - Signs of Love",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Signs of Love.ogg"
+		})
+		insert(DBM.DungeonMusic, {
+			text	= "Persona 5 - Life Will Change",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 - Life Will Change.ogg"
+		})
+		insert(DBM.DungeonMusic, {
+			text	= "Persona 5 - Rivers In the Desert",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 - Rivers In the Desert.ogg"
+		})
+		insert(DBM.DungeonMusic, {
+			text	= "Persona 5 Royal - I Believe",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 Royal - I Believe.ogg"
+		})
+		insert(DBM.DungeonMusic, {
+			text	= "Persona 5 Royal - Kichijoji 199X",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 Royal - Kichijoji 199X.ogg"
+		})
+
+	end
+
+	-- Boss BGM Table
+	if DBM.BattleMusic then
+		insert(DBM.BattleMusic, {
+			text	= "Persona 3 - Burn My Dread",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 3 - Burn My Dread.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 3 - Mass Destruction",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 3 - Mass Destruction.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 3 - When The Moon's Reaching Out Stars",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 3 - When The Moon's Reaching Out Stars.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 4 - I'll Face Myself-Battle",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - I'll Face Myself-Battle.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 4 - Pursuing My True Self",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Pursuing My True Self.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 4 - Reach Out To The Truth",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Reach Out To The Truth.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 4 - Time to Make History",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Time to Make History.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 4 - Heaven",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Heaven.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 4 - Heartbeat, Heartbreak",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Heartbeat, Heartbreak.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 4 - Signs of Love",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 4 - Signs of Love.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 5 - Last Surprise",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 - Last Surprise.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 5 - Life Will Change",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 - Life Will Change.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 5 - Rivers In the Desert",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 - Rivers In the Desert.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 5 - Will Power",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 - Will Power.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 5 Royal - I Believe",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 Royal - I Believe.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 5 Royal - Kichijoji 199X",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 Royal - Kichijoji 199X.ogg"
+		})
+		insert(DBM.BattleMusic, {
+			text	= "Persona 5 Royal - Take Over",
+			value	= "Interface\\AddOns\\DBM-PersonaMusicPack\\Music\\Persona 5 Royal - Take Over.ogg"
+		})
+		
+	end
+	inserted2 = true
+end
---
+++ README.md
@@ -0,0 +1,5 @@
+# History:
+This is built off of [DBM-SoundEventsPack](https://github.com/DeadlyBossMods/DBM-SoundEventsPack) provided by DBM. I just made made the name and file changes to add different music and sounds.
+
+# Installation:
+Just install this with your preferred addon client, or download and unzip and drop the DBM-PersonaMusicPack folder directly into addons folder and DBM-Core version 7.3.24 or higher will be able to detect and add media to Event Sound Options automatically. After that, just configure how you want.
